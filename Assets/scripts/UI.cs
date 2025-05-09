892af3de67ac4a845720009936f3040710c92728
kyoung28 <kyoung28@g.ucla.edu>
2025-05-09 13:06:19 -0700

    More UI stuff
    Adding scripts to scenes

---
+++ Assets/scripts/UI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("do thing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
