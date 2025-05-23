0d3955f9bce3c3d3954c058bf5e634849a1e7f8a
kittenluv1 <ericahuang916@gmail.com>
2025-05-23 14:26:51 -0700

    round logic

--- Assets/Scripts/RoundsScript.cs
+++ Assets/Scripts/RoundsScript.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 using Yarn.Unity;
 using UnityEngine.Video;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class RoundsScript : MonoBehaviour
 {
     public TextMeshProUGUI roundCounterUI;
     public DialogueRunner dialogueRunner;
-    public VideoPlayer videoPlayer; 
+    public VideoPlayer videoPlayer;
     public RawImage rawImage;
     public int roundCounter = 1;
+    public GameObject suspectListUI;
     public ToggleGroup suspectList;
+    public Character1Script characterScripts;
+    public Button continueButton;
+    public GameObject realSuspect;
+    public TextMeshProUGUI winOrLoseText;
     [SerializeField]
     private GameObject currentSuspect;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Dictionary<Toggle, GameObject> toggleToCharacterMap = new Dictionary<Toggle, GameObject>();
 
     void Awake()
     {
@@ -38,19 +45,83 @@ public class RoundsScript : MonoBehaviour
         videoPlayer.Prepare();
 
         // update toggle values
-        Debug.Log(suspectList.GetType());
-    }
+        GameObject[] characters = characterScripts.characters.ToArray();
+        Toggle[] toggles = suspectList.GetComponentsInChildren<Toggle>();
+        for (int i = 0; i < toggles.Length && i < characters.Length; i++)
+        {
+            Toggle toggle = toggles[i];
+            GameObject character = characters[i];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            // Use Text (legacy UI)
+            Text label = toggle.GetComponentInChildren<Text>();
+            if (label != null)
+            {
+                label.text = character.name;
+            }
+            // Add mapping
+            toggleToCharacterMap[toggle] = character;
+
+            // Add listener to assign currentSuspect when toggle is selected
+            toggle.onValueChanged.AddListener((isOn) =>
+            {
+                if (isOn)
+                {
+                    currentSuspect = character;
+                    Debug.Log("Current suspect: " + currentSuspect.name);
+                }
+                else if (currentSuspect == character)
+                {
+                    currentSuspect = null;
+                    Debug.Log("Current suspect reset to null");
+                }
+            });
+        }
+
+        // continue button
+        continueButton.onClick.AddListener(() =>
+        {
+            roundCounter++;
+            if (roundCounter > 4)
+            {
+                if (currentSuspect != null)
+                {
+                    suspectListUI.SetActive(false);
+                    endGame();
+                    return;
+                }
+                else
+                {
+                    Debug.Log("Please select a suspect.");
+                    return;
+                }
+            }
+            suspectListUI.SetActive(false);
+            roundCounterUI.text = "Round: " + roundCounter.ToString();
+        });
     }
 
     void onDialogueCompleteListener()
     {
         Debug.Log("Dialogue complete");
-        roundCounter++;
-        roundCounterUI.text = "Round: " + roundCounter.ToString();
+        suspectListUI.SetActive(true);
+    }
+
+    void endGame()
+    {
+        Debug.Log("end game called");
+        if (currentSuspect == realSuspect)
+        {
+            Debug.Log("You chose: " + currentSuspect.name + "\nYou win!");
+            winOrLoseText.text = "You chose: " + currentSuspect.name + "\nYou win!";
+            winOrLoseText.gameObject.SetActive(true);
+            winOrLoseText.enabled = true;
+        }
+        else
+        {
+            Debug.Log("You chose: " + currentSuspect.name + "\nYou lose!");
+            winOrLoseText.text = "You chose: " + currentSuspect.name + "\nYou lose!";
+            winOrLoseText.gameObject.SetActive(true);
+            winOrLoseText.enabled = true;
+        }
     }
 }
